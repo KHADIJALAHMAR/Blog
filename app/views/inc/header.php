03aa887bf650f6f02ecf04f47687e52c0204c015
khadija <khadijalahmar@gmail.com>
2021-05-31 21:21:04 +0100

    add-link-editor-summernot

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -20,6 +20,12 @@
         <script src="<?php echo URLROOT; ?>/js/jquery-3.4.1.min.js"></script> 
         <script src="<?php echo URLROOT; ?>/js/main.js"></script> 
         <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+        <!-- summernote -->
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="public/css/styles.css?v=<?php echo time();?>" rel="stylesheet" />
