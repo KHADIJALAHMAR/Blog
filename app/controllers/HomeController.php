218ca7455f11d6ab1ddd83899152aff2bfbc4c17
khadija <khadijalahmar@gmail.com>
2021-05-31 22:20:14 +0100

    creationClassHomepage_extendController_pourShowpost

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,59 +1,17 @@
 <?php
-  class ControllerCompte extends Controller {
+  class HomeController extends Controller {
     public function __construct(){
-     $this->model= $this->model('AdminCreat');
-     $this->model2= $this->model('AdminPost');
-
+     $this->model= $this->model('HomeModel');
     }
-    
-
     
     public function index(){
-    
-      $data = $this->model2->showPost();
+      
+      
+      $result = $this->model->showPost();
      
      
-      $this->view('pages/index',$data);
+      $this->view('pages/HomePage', $result);
     }
-
-
-
-
-   public function insert()
-   {
-   if (isset($_POST["submit"])) {
-     $data=[
-    'email' =>$_POST['email'],
-    'password' => $_POST['password'],
-     
-     ];
-     $this->model->signin($data);
-    header('location:'.URLROOT .'/ControllerCompte/index');
-   }
-  }
-
-
-
-  public function login(){
-    if(isset($_POST['submit'])){
-      $email= $_POST['email'];
-      $password= $_POST['password'];
-    
-  $result = $this->model->signup($email,$password);
-  if($result){
-    redirect('ControllerCompte/index');
-  }
-  
-  }
-  $this->view('pages/signup');
-  }
-
-
-
-  public function logout(){
-
-    redirect("ControllerCompte/login");
-  }
-  }
+}
 
   
