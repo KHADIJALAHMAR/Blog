ecd7428516f5f583b389540e5af7e4621c81b547
khadija <khadijalahmar@gmail.com>
2021-05-31 21:57:01 +0100

    add-modification-front-end-navbar

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,24 +1,26 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-      <a class="navbar-brand" href="<?php echo URLROOT; ?>"></a>
+      <a class="navbar-brand" href="<?php echo URLROOT; ?>">cuisine</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul class="navbar-nav mr-auto">
+      <div class="collapse navbar-collapse w-100" id="navbarsExampleDefault">
+        <ul class="navbar-nav">
           <li class="nav-item">
             <a class="nav-link" href="<?php echo URLROOT; ?>">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo URLROOT; ?>">About</a>
+            <a class="nav-link" href="<?php echo URLROOT; ?>" <?php if(!isset($_SESSION['email'])){ ?> hidden <?php }   ?>    >About</a>
           </li>
         </ul>
         
-        <ul class="navbar-nav ml-auto">
+        <ul class="navbar-nav margin-left">
             <li class="nav-item">
-              <a class="nav-link" href="<?php echo URLROOT; ?>/ControllerCompte/logout">Logout</a>
+              <a class="nav-link" href="<?php echo URLROOT; ?>/ControllerCompte/logout"  <?php if(!isset($_SESSION['email'])){ ?> style="display: none;" <?php }else{    ?> style="display: flex;" <?php } ?> >Logout</a>
             </li>
+        </ul>
+        
       </div>
     </div>
   </nav>
