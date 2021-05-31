57a6e048592983de75097c6fb25665b767bd607c
khadija <khadijalahmar@gmail.com>
2021-05-31 20:50:34 +0100

    Modification-pageIndex-Class-bootstrap

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,5 +1,8 @@
 <?php include_once APPROOT . '/views/inc/header.php'; ?>
 
+<?php if(isset($_SESSION['email'])){
+
+?>
 <?php echo APPROOT2; ?>
 <header class="masthead" style="background-image: url('../public/img/blog.png')">
     <div class="overlay"></div>
@@ -47,50 +50,52 @@
 <a href="<?php echo URLROOT; ?>/ControllerPost/insert"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"  margin-right="100"fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16" >
   <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
 </svg></a>
-<!-- card des chapitre  -->
+<div class="d-flex justify-content-center flex-wrap">
+<?php $var=0; ?>
     <?php foreach ($data as $row):?>
-    <div class="d-flex justify-content-center">
-    <div class="card" style="width: 10vh   height:10 vh ">
-    <img id="wow" src="../public/img/<?php echo $row->image ?>" width= "20px"   height="300px "class="card-img-top">
-   <div class="card-body" >
-    <h5 class="card-title"><?php echo $row->titre;?></h5>
-    
-    <!--  Model button -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-  view
-</button>
+      <div class="card mx-sm-5 my-sm-3" >
+        <img id="wow" src="../public/img/<?php echo $row->image ?>" width= "20px"   height="300px "class="card-img-top">
+      <div class="card-body" >
+        <h5 class="card-title"><?php echo $row->titre;?></h5>     
+          <!--  Model button -->
+     <button id="myBtn" class="myBtn btn btn-outline-primary" data-toggle="modal" data-target="#<?php echo $var; ?>">view</button>
 <!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel"  aria-hidden="true">
-  <div class="modal-dialog modal-dialog-scrollable">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-        <button type="button" style="width:5rem" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      <div class="modal fade" id="<?php echo $var ;?>">
+          <div class="modal-dialog modal-dialog-scrollable">
+              <div class="modal-content">
+                 <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                    <button type="button" style="width:5rem" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                </div>
+                   <div class="modal-body">
+                      <p class="card-text"><?php echo $row->contexte;?></p>
+                   </div>
+                        <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                              <button type="button" class="btn btn-primary">Understood</button>
+                        </div>
+              </div>
+          </div>
       </div>
-      <div class="modal-body">
-      <p class="card-text"><?php echo $row->contexte;?></p>
+
+          <a href="<?php echo URLROOT;?>/ControllerPost/delete?id=<?php echo $row->id;?>"class="btn btn-danger " >Delet</a>
+          <a href="<?php echo URLROOT;?>/ControllerPost/update/<?php echo $row->id;?>"class="btn btn-success " >update</a>
+        </div>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Understood</button>
-      </div>
-    </div>
-  </div>
-</div>
-    <!-- <p class="card-text"><?php echo $row->contexte;?></p> -->
-    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-    
-    <a href="<?php echo URLROOT;?>/ControllerPost/delete?id=<?php echo $row->id;?>"class="btn btn-danger " >Delet</a>
-    <a href="<?php echo URLROOT;?>/ControllerPost/update/<?php echo $row->id;?>"class="btn btn-success " >update</a>
-  </div>
-</div>
-      </div>
-      </div>
+    <?php $var++ ; ?>
     <?php endforeach;?>
+</div>
+
+
+  <?php include_once APPROOT . '/views/inc/footer.php'; ?>
+  <?php } else {
+    header("location: /exempl/ControllerCompte/login");  
+  }
+
+  ?>
     </div>
   </div> 
 
-  
   
 
 
