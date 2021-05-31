3cf440e34b3eb5db2a2d03e04b5dfdb27251a69f
khadija <khadijalahmar@gmail.com>
2021-05-31 21:47:22 +0100

    add_cards_HomePage

--- app/views/pages/HomPage.php
+++ app/views/pages/HomPage.php
@@ -6,3 +6,24 @@
     <video id="Mp4Video-1590159698427" class="myVideo1" src="img/bgvideo2.mp4"  loop="" muted="muted" playsinline="" autoplay="" style="width: 98vw; opacity: 1;"></video>
   </div>
 </header>
+<div class="d-flex justify-content-center">
+<div class="bs-example">
+    <div class="card" style="max-width: 500px;">
+        <div class="row no-gutters">
+            <div class="col-sm-5" style="background: #868e96;">
+                <img src="img/h.webp" class="card-img-top h-100" alt="...">
+            </div>
+            <div class="col-sm-7">
+                <div class="card-body">
+                    <h1 class="card-title">Alice Liddel</h1>
+                    <p class="card-text">Bienvenue sur le blog de cuisine d'e-monsite !
+                          Ce blog est un site de démonstration qui regroupe des recettes de cuisine
+                          issues de la communauté d'utlisateurs d'e-monsite. Le module blog permet
+                          de créer autant d'articles que vous.</p>
+                    <a href="#" class="btn btn-primary stretched-link">View Profile</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
