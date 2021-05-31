1bb12fcae25494c1230d65e80d0c79c7628103c0
khadija <khadijalahmar@gmail.com>
2021-05-31 21:45:42 +0100

    includ_header_ajouter_vidéo

---
+++ app/views/pages/HomPage.php
@@ -0,0 +1,8 @@
+<?php include_once APPROOT . '/views/inc/header.php';
+ ?>
+<header class="masthead">
+  <div>
+  <!--video  header-->
+    <video id="Mp4Video-1590159698427" class="myVideo1" src="img/bgvideo2.mp4"  loop="" muted="muted" playsinline="" autoplay="" style="width: 98vw; opacity: 1;"></video>
+  </div>
+</header>
