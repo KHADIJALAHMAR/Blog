101a3131276246e4f0057827439b2427c44495aa
khadija <khadijalahmar@gmail.com>
2021-05-31 22:14:22 +0100

    instantion-database-query_SElECT-HomeModel

--- app/models/HomeModel.php
+++ app/models/HomeModel.php
@@ -1,4 +1,20 @@
+<?php
+class HomeModel{
+    private $db;
 
+    public function __construct(){
+      $this->db = new Database;
+    }    
+
+    public function showPost()
+    {
+      $this->db->query("SELECT * FROM `post`");
+
+      $results = $this->db-> resultSet();
+
+      return $results;
+    }
+}
 
   
     
