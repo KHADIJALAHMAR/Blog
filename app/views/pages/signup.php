acb8a147e45c18bcb5fef13eeed51e9d5b3cd7b2
khadija <khadijalahmar@gmail.com>
2021-05-31 21:33:59 +0100

    includ_header_ajouter_section_signup

--- app/views/pages/signup.php
+++ app/views/pages/signup.php
@@ -1,18 +1,17 @@
-<?php require APPROOT . '/views/inc/header.php'; ?>
-<body>
-<form action="<?php echo URLROOT; ?>/ControllerCompte/login" method="POST">
-  <div class="form-row">
-    <div class="col">
-      <input type="text" class="form-control" name="email" placeholder="email">
-    </div>
-    <div class="col">
-      <input type="password" class="form-control"  name="password" placeholder="password">
-    </div>
-    <div class="form-group">
-        <input type="submit" class="btn btn-primary" value="enter" name="submit">
-        </div>
-  </div>
-  <?php require APPROOT . '/views/inc/footer.php'; ?>
-</form>
+<?php require APPROOT . '/views/inc/header.php';
+ ?>
 </body>
+
 </html>
+
+<section class="Form my-4 mx-5">
+    <div class="container">
+        <div class="row no-gutters rounded ">
+            <div class="col-lg-5 shadow">
+                <img src="../public/img/halwa.jpg" alt="loginimg" class="img-fluid">
+            </div>
+            
+            <div class="col-lg-7 px-5 shadow-lg p-3 mb-6  bg-white rounded">
+                <div class="col-lg-12 d-flex justify-content-center py-3 ">
+                    <img src="../public/img/logo.png" alt="shieldimg" class="img-fluid">
+                </div>
